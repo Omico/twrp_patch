9a5f0915de24d687ad2328672d71b1237c2728c9
Omico <omico@qq.com>
2021-05-25 16:45:05 -0700

    Update apply-build-recovery-installer.sh

--- apply-build-recovery-installer.sh
+++ apply-build-recovery-installer.sh
@@ -3,5 +3,5 @@
 source_root=$(cd "$(dirname "$0")"; cd ..; pwd)
 
 cd $source_root/build/make
-git reset --hard omnirom/android-9.0
+git reset --hard $1
 git fetch "https://gerrit.omnirom.org/android_build" refs/changes/82/33182/5 && git cherry-pick FETCH_HEAD
